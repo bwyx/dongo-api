406b03f18870213ab983f217d9ca15ad1b2806a4
Bayu Kurnia <bayukapp@gmail.com>
2021-06-09 13:47:10 +0700

    validate env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,24 @@
+import Joi from 'joi';
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    PORT: Joi.number().default(3000),
+    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
 export default {
-  env: process.env.NODE_ENV as string,
-  port: Number(process.env.PORT),
+  env: envVars.NODE_ENV as string,
+  port: Number(envVars.PORT),
   mongoose: {
-    url: process.env.MONGODB_URL as string,
+    url: envVars.MONGODB_URL as string,
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,

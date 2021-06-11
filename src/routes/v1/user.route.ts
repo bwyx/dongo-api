89b3cc0d254b1c2a01c1367a1bf51b4a400d8691
Bayu Kurnia <bayukapp@gmail.com>
2021-06-11 12:34:42 +0700

    fix route

--- src/routes/v1/user.route.ts
+++ src/routes/v1/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { validate } from '../../middlewares';
+import { auth, validate } from '../../middlewares';
 import { userValidation } from '../../validations';
 import { userController } from '../../controllers';
 
@@ -9,8 +9,8 @@ router.route('/').post(validate(userValidation.createUser), userController.creat
 
 router
   .route('/:userId')
-  .get(validate(userValidation.getUser), userController.getUser)
-  .patch(validate(userValidation.updateUser), userController.updateUser)
-  .delete(validate(userValidation.deleteUser), userController.deleteUser);
+  .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
+  .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
+  .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
 
 export default router;
